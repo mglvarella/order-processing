64478ba270d72310efc67639bc0a912e3585b53b
mglvarella <mig-varella1@hotmail.com>
2025-11-30 22:24:10 -0300

    Created OrderDTO

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,30 @@
-export class CreateOrderDto {}
+import { Type } from 'class-transformer';
+import { IsArray, IsDateString, IsNotEmpty, IsNumber, ValidateNested } from 'class-validator';
+
+class CreateItemDto {
+    @IsNumber()
+    @IsNotEmpty()
+    productId: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    quantity: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    price: number;
+}
+
+export class CreateOrderDto {
+    @IsNumber()
+    @IsNotEmpty()
+    value: number;
+
+    @IsDateString()
+    creationDate: string;
+
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => CreateItemDto)
+    items: CreateItemDto[];
+}
